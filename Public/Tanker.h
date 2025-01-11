29868d191f83eb7a3ef93875a64ec836229a444d
Jihyeon0 <wjswlqhd1@naver.com>
2025-01-11 17:20:06 +0900

    add : 몬스터, 자식몬스터 클래스 분리, 상대경로 수정
    링커, c/c++의 추가포함디렉토리에 상대경로 ./Public 으로 수정

---
+++ Public/Tanker.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "Monster.h"
+
+class Tanker : public Monster
+{
+public:
+    Tanker(string Monstername, int MonsterHP, int MonsterEXP, int MonsterGold, int MonsterAttack) // 생성자 호출 순서: 부모 생성자 호출 -> 부모 생성자에서 멤버변수 초기화 -> 부모 생성자 호출 종료 -> 자식 생성자 호출  -> 자식 생성자 호출 종료
+        :Monster(Monstername, MonsterHP, MonsterEXP, MonsterGold, MonsterAttack)
+    {
+        //기타 초기 필요 초기화 
+    }
+    ~Tanker() {}
+
+public:
+};
