b195cbb1f74b6bb462575290f1972e12441539d0
Jihyeon0 <wjswlqhd1@naver.com>
2025-01-12 22:51:36 +0900

    add: 던전 매니저, 힐러, 캐릭터 클래스

---
+++ Private/DungeonManager.cpp
@@ -0,0 +1,61 @@
+﻿#include "DungeonManager.h"
+#include "MapManager.h"
+#include "Character.h"
+#include "Tanker.h"
+#include "Healer.h"
+
+void DungeonManager::EnterDungeon(Character* Player, MapManager* MM)
+{
+	while (!isDungeonEnd) //플레이어가 죽거나, 던전 클리어하면 종료
+	{
+		MM->MapPrint();
+		//monster = new Tanker("탱커", 100, 100, 0, 5, 10);
+		monster = new Healer("힐러", 100, 100, 0, 5, 10);
+		Player->PrintCharacter();
+		monster->PrintMonster();
+
+			//던전 안에 맵 1칸 단위 싸움
+		eFightResult = Fight(Player, MM);
+
+		switch (eFightResult)
+		{
+		case PLAYERWIN:
+		{
+			//다음 스테이지로 이동
+			cout << "플레이어 승리! 다음 스테이지로 이동!" << endl;
+		}
+		break;
+		case PLAYERLOSE:
+		{
+			//던전 종료
+		}
+		break;
+		default:
+			break;
+		}
+
+		//종료시 처리
+		isDungeonEnd = true;
+		delete monster;
+		monster = nullptr;
+		//사용했던 던전 vertor 정리 
+	}
+
+}
+
+FightResult DungeonManager::Fight(Character* Player, MapManager* MM)
+{
+	while (true) //플레이어가 죽거나, 던전 클리어하면 종료
+	{
+		if (Player->GetHp() > 0 && monster->GetHp() > 0)
+		{
+			cout << Player->GetName()<< "(이)가 " << Player->GetAttack() << "만큼 " << monster->GetName() << "(을)를 공격했다!" << endl;
+			monster->TakeDamage(Player->GetAttack());
+			cout << monster->GetName() << "에게 " << Player->GetAttack() << "만큼 피해를 입혔다!" << endl;
+			cout << Player->GetName() << " 체력 : "<< Player->GetHp()<< ", "<< monster->GetName() << " 체력 : " << monster->GetHp() << endl;
+			
+		}
+		if (monster->GetHp() <= 0)
+			return PLAYERWIN;
+	}
+}
---
+++ Public/Character.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Character
+{
+public:
+    Character(string Charactername, int CharacterHP, int CharacterEXP, int CharacterGold, int CharacterAttack)
+    :_Charactername(Charactername), _CharacterHP(CharacterHP), _CharacterEXP(CharacterEXP), _CharacterGold(CharacterGold), _CharacterAttack(CharacterAttack), _Damage(0)
+    {
+
+    }
+	~Character() {}
+public: 
+    string GetName() { return _Charactername; }
+    int GetHp() { return _CharacterHP; }
+    int GetAttack() { return _CharacterAttack; }
+    void TakeDamage(int damage)
+    {
+        _Damage = damage;
+        _CharacterHP -= _Damage;
+        if (_CharacterHP <= 0) { _CharacterHP = 0; return; } //피격 당한 후 hp 체크 -> 몬스터 죽으면 삭제
+        //삭제 추가 필요
+    }
+
+public: //테스트를 위한 
+    void CharacterSkill() { cout << "캐릭터 스킬 사용, 아무것도 없음" << endl; }
+    void PrintCharacter() const;
+
+public:
+    string _Charactername;
+    int _CharacterHP;
+    int _CharacterEXP;
+    int _CharacterGold;
+    int _CharacterAttack;
+    int _Damage; //받은 데미지 저장을 위한 변수
+};
