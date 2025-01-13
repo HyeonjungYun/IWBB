e6d492b7d0bcda35db8a442633b20fb5152e226f
윤현중 <58295105+HyeonjungYun@users.noreply.github.com>
2025-01-13 18:20:20 +0900

    🔥Del : GameManager헤더에 멤버 함수 삭제

--- Public/GameManager.h
+++ Public/GameManager.h
@@ -6,6 +6,7 @@
 #include "DungeonManager.h"
 #include "StoreManager.h"
 
+// 게임 진행을 총책하는 클래스
 class GameManager
 {
 public:
@@ -17,54 +18,5 @@ private:
 	//DungeonManager* _DungeonManager;
 	StoreManager* _StoreManager;
 };
-
-GameManager::GameManager()
-{
-	string name;
-	cout << "캐릭터 이름을 입력하세요. : ";
-	getline(cin, name);
-
-	cout << "BB 던전에 입장하신 것을 환영합니다." << endl;
-
-	_Character = new Character(name);
-	//_DungeonManager = new DungeonManager;
-	_StoreManager = new StoreManager(_Character);
-}
-
-void GameManager::GamePlaying()
-{
-	while (1)
-	{
-		string choice;
-
-		cout << "아래로 내려가는 계단이 보인다." << endl;
-		cout << "-------------------------------------------" << endl;
-		_Character->PrintCharacter();
-		cout << "-------------------------------------------" << endl;
-		cout << "1. 계단을 내려간다." << endl;
-		cout << "2. 상점을 이용한다." << endl;
-		cout << "3. 게임을 종료한다." << endl;
-		cout << "입력 : ";
-		getline(cin, choice);
-
-		if (choice == "1")
-		{
-			// 던전 입장
-		}
-		if (choice == "2")
-		{
-			_StoreManager->VisitStore();
-		}
-		if (choice == "3")
-		{
-			break;
-		}
-	}
-}
-
-GameManager::~GameManager()
-{
-	delete this;
-}
 
 #endif
