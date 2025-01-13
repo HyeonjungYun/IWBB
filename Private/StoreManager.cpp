c2df81bbf181269aa09b14edc3d895fe80166839
윤현중 <58295105+HyeonjungYun@users.noreply.github.com>
2025-01-13 16:43:44 +0900

    🩹Fix : StoreManager 출력문 변경

--- Private/StoreManager.cpp
+++ Private/StoreManager.cpp
@@ -12,6 +12,13 @@ void StoreManager::VisitStore()
 		cout << "상점" << endl;
 		cout << "상품 구매" << endl;
 		cout << "1. HP포션\n2.레벨업권\n3.용기의 박스\n4.상점 나가기" << endl;
+		cout << "-------------------------------------------" << endl;
+		cout << "현재 레벨업권을 구매하시면 " << (_Character->_CharacterEXP / 100) << "레벨업 가능합니다." << endl;
+		cout << "HP가 " << (_Character->_CharacterEXP / 100) << " x 20만큼 증가합니다." << endl;
+		cout << "공격력이 " << (_Character->_CharacterEXP / 100) << " x 20만큼 증가합니다." << endl;
+			//_CharacterHP += ((_CharacterEXP / 100) * 2) * 20;
+			//_CharacterAttack += ((_CharacterEXP / 100) * 2) * 20;
+		cout << "-------------------------------------------" << endl;
 		cout << "원하시는 번호를 입력하세요. :";
 		string choice;
 		getline(cin, choice);
