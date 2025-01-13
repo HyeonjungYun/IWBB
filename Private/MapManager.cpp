5ca139b61861782a1923012c30ca664c36bba798
윤현중 <58295105+HyeonjungYun@users.noreply.github.com>
2025-01-13 10:52:29 +0900

    🩹 Fix : 맵 매니저 클래스  변수명 변경

--- Private/MapManager.cpp
+++ Private/MapManager.cpp
@@ -2,39 +2,39 @@
 
 MapManager::MapManager(int mapLevel)	// 맵 생성자, 맵을 기본적인 맵을 생성, 시작 좌표와 끝 좌표를 생성 후 할당
 {
-	_mapLevel = mapLevel;
+	_MapLevel = mapLevel;
 
-	vector<vector<char>> tempMap(_mapLevel + 2, vector<char>(_mapLevel + 2, ' '));
-	vector<vector<char>> tempClearedMap(_mapLevel + 2, vector<char>(_mapLevel + 2, ' '));
+	vector<vector<char>> tempMap(_MapLevel + 2, vector<char>(_MapLevel + 2, ' '));
+	vector<vector<char>> tempClearedMap(_MapLevel + 2, vector<char>(_MapLevel + 2, ' '));
 
-	_characterPos = RandomPos();
-	_endPos = RandomPos();
+	_CharacterPos = RandomPos();
+	_EndPos = RandomPos();
 
-	tempMap[_endPos.y][_endPos.x] = 'E';
-	tempMap[_characterPos.y][_characterPos.x] = '!';
-	tempClearedMap[_endPos.y][_endPos.x] = 'C';
-	tempClearedMap[_characterPos.y][_characterPos.x] = 'C';
+	tempMap[_EndPos.y][_EndPos.x] = 'E';
+	tempMap[_CharacterPos.y][_CharacterPos.x] = '!';
+	tempClearedMap[_EndPos.y][_EndPos.x] = 'C';
+	tempClearedMap[_CharacterPos.y][_CharacterPos.x] = 'C';
 
-	_map = tempMap;
-	_clearedMap = tempClearedMap;
+	_Map = tempMap;
+	_ClearedMap = tempClearedMap;
 }
 
 void MapManager::MapPrint() const	// 현재 맵을 프린트
 {
 	cout << "-";
 
-	for (vector<char> row : _map)
+	for (vector<char> row : _Map)
 		cout << "----";
 	cout << endl;
 
-	for (vector<char> row : _map)
+	for (vector<char> row : _Map)
 	{
 		cout << "|";
 		for (char room : row)
 			cout << " " << room << " |";
 
 		cout << endl << "-";
-		for (vector<char> row : _map)
+		for (vector<char> row : _Map)
 			cout << "----";
 		cout << endl;
 	}
@@ -42,24 +42,24 @@ void MapManager::MapPrint() const	// 현재 맵을 프린트
 
 void MapManager::SetCharacterPosition(int x, int y)	// 현재 캐릭터가 위치한 곳을 수정 
 {
-	_characterPos.x = x;
-	_characterPos.y = y;
+	_CharacterPos.x = x;
+	_CharacterPos.y = y;
 
-	_map[_endPos.y][_endPos.x] = 'E';
-	_map[_characterPos.y][_characterPos.x] = '!';
+	_Map[_EndPos.y][_EndPos.x] = 'E';
+	_Map[_CharacterPos.y][_CharacterPos.x] = '!';
 }
 
 Pos MapManager::GetCharacterPosition() const
 {
-	return _characterPos;
+	return _CharacterPos;
 }
 
 void MapManager::RenewDungeonStatus()
 {
-	_map = _clearedMap;
+	_Map = _ClearedMap;
 	
-	_map[_endPos.x][_endPos.y] = 'E';
-	_map[_characterPos.x][_characterPos.y] = '!';
+	_Map[_EndPos.x][_EndPos.y] = 'E';
+	_Map[_CharacterPos.x][_CharacterPos.y] = '!';
 }
 
 const Pos MapManager::RandomPos() const	// 랜덤한 좌표를 생성하는 함수
@@ -68,7 +68,7 @@ const Pos MapManager::RandomPos() const	// 랜덤한 좌표를 생성하는 함수
 
 	random_device rd;
 	mt19937 gen(rd());
-	uniform_int_distribution<> dist(0, _mapLevel + _width - 1);
+	uniform_int_distribution<> dist(0, _MapLevel + _Width - 1);
 
 	temp.x = dist(gen);
 	temp.y = dist(gen);
@@ -78,13 +78,13 @@ const Pos MapManager::RandomPos() const	// 랜덤한 좌표를 생성하는 함수
 
 bool MapManager::IsDungeonClear() const	// 현재 캐릭터 위치와 끝 지점의 위치가 동일한지 확인
 {
-	if (_characterPos.x == _endPos.x && _characterPos.y == _endPos.y) return true;
+	if (_CharacterPos.x == _EndPos.x && _CharacterPos.y == _EndPos.y) return true;
 	return false;
 }
 
 void MapManager::RoomClear()
 {
-	_clearedMap[_characterPos.x][_characterPos.y] = 'C';
+	_ClearedMap[_CharacterPos.x][_CharacterPos.y] = 'C';
 }
 
 MapManager::~MapManager()	// 소멸자
