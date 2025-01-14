0bbf490911a84f4f86a97781a46dc28ac55c7d48
윤현중 <58295105+HyeonjungYun@users.noreply.github.com>
2025-01-14 22:31:33 +0900

    ✨Update : Healer 작업 결과물 다듬기

--- Private/Healer.cpp
+++ Private/Healer.cpp
@@ -1,22 +1,18 @@
 ﻿#include "Healer.h"
 
-
-void Healer::PrintNothingHappened()
+Healer::Healer(int MapLevel)
 {
-	cout << "이미 체력이 차 있어 힐을 할 수 없었다." << endl;
+	random_device rd; //랜덤 생성 클래스 선언
+	mt19937 gen(rd()); // 난수
+	uniform_int_distribution<> dist(0, 1);
+	int randomValue = dist(gen); // 0,1;
+	if (randomValue == 1)
+		_Monstername = "메르시";
+	else
+		_Monstername = "소나";
 }
 
-void Healer::SetIgnoreDamage(int damage)
-{
-	IgnoreDamage = damage;
-}
-
-float Healer::GetIgnoreRate() const
-{
-	return IgnoreRate;
-}
-
-bool Healer::CanIgnore() //주석
+bool Healer::CanIgnore()
 {
 	int randomValue = rand() % 2;
 	if (randomValue == 1)
@@ -31,45 +27,34 @@ bool Healer::CanIgnore() //주석
 
 void Healer::TakeDamage(int damage)
 {
-	int randomValue = rand() % 2;
-	if (randomValue == 1)
+	random_device rd; //랜덤 생성 클래스 선언
+	mt19937 gen(rd()); // 난수
+	uniform_int_distribution<> dist(0, 10);
+	
+	if (dist(gen) < 4)
 	{
-		SetIgnoreDamage(damage);
-		cout << /*힐러 몬스터 이름*/ "이(가) 공격을 무시했다." << endl;
+		cout << _Monstername << "이(가) 공격을 무시했다." << endl;
 	}
 	else
 	{
-		_MonsterHP -= damage; // 현재 체력 -=데미지
+		_MonsterHP -= damage; // 현재 체력 -= 데미지
 	}
 }
 
-
-	bool Healer::Canskill() // 반환값이 없는 bool인 고유 스킬 호출하는 곳
+void Healer::IncreaseHP() // 상위클래스에서 currentHP, maxHP 필요
+{
+	if (_MonsterHP >= _MonsterMaxHP)
 	{
-		return true;
+		return;
 	}
 
-	void Healer::IncreaseHP() // 상위클래스에서 currentHP, maxHP 필요
+	int increaseHPAmount = _MonsterHP / 10; // 고정된 회복량 1
+	_MonsterHP += increaseHPAmount; // 체력 증가
+
+	if (_MonsterHP > _MonsterMaxHP)
 	{
-		if (_MonsterHP >= _MonsterMaxHP)
-		{
-			PrintNothingHappened();
-			return;
-		}
-
-		int increaseHPAmount = 1; // 고정된 회복량 1
-		_MonsterHP += increaseHPAmount; // 체력 증가
-
-		if (_MonsterHP > _MonsterMaxHP)
-		{
-			_MonsterHP = _MonsterMaxHP;
-		}
-
-		cout <<  /*힐러 몬스터 이름*/ "이(가) 체력을 " << increaseHPAmount << "만큼 회복했다." << endl;
+		_MonsterHP = _MonsterMaxHP;
 	}
 
-
-	void Healer::Skill() // 반환값이 없는 고유 스킬 호출하는 곳
-	{
-		IncreaseHP(); // 체력 회복 스킬 사용
-	}
+	cout << _Monstername << "이(가) 체력을 " << increaseHPAmount << "만큼 회복했다." << endl;
+}
--- Public/Healer.h
+++ Public/Healer.h
@@ -1,31 +1,15 @@
-﻿#pragma once
+﻿#ifndef _HEALER_H_
+#define _HEALER_H_
+
 #include "Monster.h"
 
 class Healer : public Monster
 {
 public:
-    Healer(string Monstername, int MonsterMaxHP, int MonsterHP, int MonsterEXP, int MonsterGold, int MonsterAttack) // 생성자 호출 순서: 부모 생성자 호출 -> 부모 생성자에서 멤버변수 초기화 -> 부모 생성자 호출 종료 -> 자식 생성자 호출  -> 자식 생성자 호출 종료
-        :Monster(Monstername, MonsterMaxHP, MonsterHP, MonsterEXP, MonsterGold, MonsterAttack), IgnoreDamage(0), IgnoreRate(0.5f)
-    {
-        //기타 초기 필요 초기화 
-    }
-    ~Healer() { delete this; }
-
-public:
-    void PrintNothingHappened(); // 고유 스킬 : 무시 사용 -> 로그매니저를 이용해 출력할 문자열 출력 함수
-    void SetIgnoreDamage(int damage); // 데미지 무시 값 = 피해 받은 데미지
-    float GetIgnoreRate() const; // 무시 확률 1/2 고정
-
-public:
+    Healer(int MapLevel);
     void TakeDamage(int damage) override;
-    void Skill() override;
-    bool Canskill() override;
-
-private:
     bool CanIgnore(); // 힐러 고유 스킬 : 무시, 받는 공격을 무시함
     void IncreaseHP(); // 힐러 기본 스킬 : 힐, 체력을 회복함. 회복량은 1로 고정
-
-private:
-    float IgnoreRate; // 공격 무시 스킬 실행 확률
-    int IgnoreDamage; // 무시한 데미지 값
 };
+
+#endif
